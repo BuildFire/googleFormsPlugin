33e57ff8ce6c1fd5aaf00fdcce090bc706897b72
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-02-08 15:20:24 +0530

    Showed google form with given url in widget section

--- widget/app.js
+++ widget/app.js
@@ -5,6 +5,30 @@
     .controller('WidgetHomeCtrl', ['$scope', 'Buildfire', 'DataStore', 'TAG_NAMES', 'STATUS_CODE',
       function ($scope, Buildfire, DataStore, TAG_NAMES, STATUS_CODE) {
         var WidgetHome = this;
+        /*
+         * Fetch user's data from datastore
+         */
+        WidgetHome.init = function () {
+          WidgetHome.success = function (result) {
+            WidgetHome.data = result.data;
+            if (!WidgetHome.data.content)
+              WidgetHome.data.content = {};
+            console.log(">>>>>", WidgetHome.data);
+          };
+          WidgetHome.error = function (err) {
+            if (err && err.code !== STATUS_CODE.NOT_FOUND) {
+              console.error('Error while getting data', err);
+            }
+          };
+          DataStore.get(TAG_NAMES.GOOGLE_FORM_INFO).then(WidgetHome.success, WidgetHome.error);
+        };
+
+        WidgetHome.init();
 
       }])
+    .filter('returnUrl', ['$sce', function ($sce) {
+      return function (url) {
+        return $sce.trustAsResourceUrl(url);
+      }
+    }]);
 })(window.angular);
